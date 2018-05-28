c137ddd6656904d57dca1ef3631b087a1c0953c4
alexi430 <37286391+alexi430@users.noreply.github.com>
2018-05-28 14:39:02 +0800

    change unity 2017
    change unity 2017

--- u.bat
+++ u.bat
@@ -1,6 +1,6 @@
 @echo off 
 set WS=%~dp0
-set UNITY_PATH="C:\Program Files\Unity\Editor\Unity.exe"
+set UNITY_PATH="C:\Program Files\Unity2017\Editor\Unity.exe"
 set PROJECT_PATH="%WS%"
 set BUILD_LOG_PATH="%WS%build.log"
 set DESTINATION_PATH="%WS%build"
