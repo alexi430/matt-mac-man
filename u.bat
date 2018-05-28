5ed4763f3e89e880dc47944482606818f4e9a84d
alexi430 <37286391+alexi430@users.noreply.github.com>
2018-05-28 16:13:46 +0800

    put log back to build

--- u.bat
+++ u.bat
@@ -6,4 +6,4 @@ set BUILD_LOG_PATH="%WS%build.log"
 set DESTINATION_PATH="%WS%build"
 del "%WS%build\NumbersFlip.apk"
 echo Build Command: %UNITY_PATH% -quit -batchmode -projectPath %PROJECT_PATH% -executeMethod BuildTool.Build -logFile %BUILD_LOG_PATH% -destinationPath %DESTINATION_PATH%
-%UNITY_PATH% -quit -batchmode -projectPath %PROJECT_PATH% -executeMethod BuildTool.Build -destinationPath %DESTINATION_PATH%
+%UNITY_PATH% -quit -batchmode -projectPath %PROJECT_PATH% -executeMethod BuildTool.Build -logFile %BUILD_LOG_PATH% -destinationPath %DESTINATION_PATH%
