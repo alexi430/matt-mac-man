cfb7b565170deb3e7be5295802978d1a08c4476a
alexi430 <37286391+alexi430@users.noreply.github.com>
2018-05-28 19:25:48 +0800

    add space

--- t.bat
+++ t.bat
@@ -2,5 +2,5 @@
 SET WS=%~dp0
 SET UNITY="C:\Program Files\Unity2017\Editor\Unity.exe"
 SET PROJECT="%WS%"
-SET RESULT="%WS%result.xml"
+SET RESULT="%WS%result.xml" 
 %UNITY% -batchmode -runTests -projectPath %PROJECT% -testResults %RESULT% -testPlatform editmode
