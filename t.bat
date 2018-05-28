2239aa3f30d97c4a803636e422bc56386beeb2f0
alexi430 <37286391+alexi430@users.noreply.github.com>
2018-05-28 15:36:29 +0800

    fix test runner result path

--- t.bat
+++ t.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET UNITY="C:\Program Files\Unity2017\Editor\Unity.exe"
 SET PROJECT=%~dp0
-SET RESULT="%PROJECT%..\results.xml"
+SET RESULT="%PROJECT%results.xml"
 %UNITY% -batchmode -runTests -projectPath %PROJECT% -testResults %RESULT% -testPlatform editmode
