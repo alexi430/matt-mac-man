20b9d7f088aee6b0884e7287961b96b779a4e39e
alexi430 <37286391+alexi430@users.noreply.github.com>
2018-05-28 15:46:50 +0800

    fix result path

--- t.bat
+++ t.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
+SET WS=%~dp0
 SET UNITY="C:\Program Files\Unity2017\Editor\Unity.exe"
-SET PROJECT=%~dp0
-SET RESULT="%PROJECT%results.xml"
+SET PROJECT="%WS%"
+SET RESULT="%WS%result.xml"
 %UNITY% -batchmode -runTests -projectPath %PROJECT% -testResults %RESULT% -testPlatform editmode
